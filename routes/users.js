372ebf5e847a52090c032be03030bf850d071b2e
AnhTRJotnar <109334995+AnhTRJotnar@users.noreply.github.com>
2024-03-17 18:47:58 +0700

    userlist

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', async(req, res) {
+  var UserList = await UserModel.find({});
+    res.render('/', { UserList })
 });
 
 module.exports = router;
