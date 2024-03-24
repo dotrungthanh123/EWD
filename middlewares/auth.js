d7dc4db89a7ed04db6d5ba3046f51658519ea511
AnhTRJotnar <109334995+AnhTRJotnar@users.noreply.github.com>
2024-03-25 01:35:33 +0700

    Change authentication

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,80 +6,32 @@ const checkLoginSession = (req, res, next) => {
     }
 }
 
-const checkMktCoordinatorITSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'mktCoordinatorIT') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkMktCoordinatorDesignSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'mktCoordinatorDesign') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkMktCoordinatorBusinessSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'mktCoordinatorBusiness') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkStudentITSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'studentIT') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkStudentDesignSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'studentDesign') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkStudentBusinessSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'studentBusiness') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkGuestITSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'guestIT') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkGuestDesignSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'guestDesign') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
-const checkGuestBusinessSession = (req, res, next) => {
-    if (req.session.username && req.session.role === 'guestBusiness') {
-        next();
-    } else {
-        res.redirect('/auth/login');
-    }
-};
-
 const checkMktManagerSession = (req, res, next) => {
     if (req.session.username && req.session.role === 'mktManager') {
+        next();
+    } else {
+        res.redirect('/auth/login');
+    }
+};
+
+const checkMktCoordinatorSession = (req, res, next) => {
+    if (req.session.username && req.session.role === 'mktCoordinator') {
+        next();
+    } else {
+        res.redirect('/auth/login');
+    }
+};
+
+const checkStudentSession = (req, res, next) => {
+    if (req.session.username && req.session.role === 'student') {
+        next();
+    } else {
+        res.redirect('/auth/login');
+    }
+};
+
+const checkGuestSession = (req, res, next) => {
+    if (req.session.username && req.session.role === 'guest') {
         next();
     } else {
         res.redirect('/auth/login');
@@ -89,13 +41,7 @@ const checkMktManagerSession = (req, res, next) => {
 module.exports = {
     checkLoginSession,
     checkMktManagerSession,
-    checkMktCoordinatorITSession,
-    checkMktCoordinatorDesignSession,
-    checkMktCoordinatorBusinessSession,
-    checkStudentITSession,
-    checkStudentDesignSession,
-    checkStudentBusinessSession,
-    checkGuestITSession,
-    checkGuestDesignSession,
-    checkGuestBusinessSession
+    checkMktCoordinatorSession,
+    checkStudentSession,
+    checkGuestSession
 };
