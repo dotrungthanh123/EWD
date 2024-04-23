abea41a48079e8a3b86cacb5891756d3a9e4f78d
AnhTRJotnar <109334995+AnhTRJotnar@users.noreply.github.com>
2024-04-24 03:51:25 +0700

    Some stats

---
+++ public/javascripts/stats.js
@@ -0,0 +1,23 @@
+// const ctx = document.getElementById('contributionsByFacultyChart').getContext('2d');
+// const chartData = {
+//     labels: ['Label1', 'Label2', 'Label3'],
+//     datasets: [{
+//         label: 'Example Data',
+//         data: [10, 20, 30],
+//         backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+//         borderColor: ['rgba(255, 99, 132, 1)'],
+//         borderWidth: 1
+//     }]
+// };
+
+// new Chart(ctx, {
+//     type: 'bar',
+//     data: chartData,
+//     options: {
+//         scales: {
+//             y: {
+//                 beginAtZero: true
+//             }
+//         }
+//     }
+// });
